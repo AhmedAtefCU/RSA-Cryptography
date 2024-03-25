67dbf91fafd8e5374fd9f557dcc49727d236a7ea
Ahmed Atef <AA2201416@tkh.edu.eg>
2024-03-25 12:48:27 +0200

    Update RSA Pseudocode.py

--- RSA Pseudocode.py
+++ RSA Pseudocode.py
@@ -1,2 +1,13 @@
-import random
-import math 
+function is_prime(n):
+    if n <= 1:
+        return False
+    else if n <= 3:
+        return True
+    else if n is divisible by 2 or 3:
+        return False
+    i = 5
+    while i * i <= n:
+        if n is divisible by i or n is divisible by (i + 2):
+            return False
+        i += 6
+    return True
