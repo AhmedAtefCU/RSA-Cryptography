da131d58f7b9b9d4393e6653ff9c43b10651f867
Ahmed Atef <AA2201416@tkh.edu.eg>
2024-04-12 18:57:52 +0200

    Update RSA Python Code.py

--- RSA Python Code.py
+++ RSA Python Code.py
@@ -23,4 +23,31 @@ def mod_inverse(a, m):
     else:
         return x % m
     
+def probable_prime_numbers(n, k=10):
+    # Checks if a Number is a Probable Prime Using the Miller-Rabin Primality Test 
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
 
